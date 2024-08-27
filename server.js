0f52cf0bee12e05b0ecaa5ee26c2434caf2fd5ee
pro <pro@pros-MacBook-Pro.local>
2024-08-28 01:36:13 +0530

    Your commit message

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('ice-candidate', (candidate) => {
+    socket.broadcast.emit('ice-candidate', candidate);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Signaling server running on port 3000');
+});
+
+
+
